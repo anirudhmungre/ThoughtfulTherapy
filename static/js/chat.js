34682bd6d5d2ba1f7b5efdd24b9101c09608699f
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-22 19:04:13 -0500

    document static

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,5 +1,6 @@
-
 const addMessage = (text, time, userMessage) => {
+    // This function manipulates the DOM to add a new message in the message box element
+    // Everything is general formatting for the messages
     let datetime = new Date(time);
     let messageBox = document.getElementById('message-box');
 
@@ -12,6 +13,7 @@ const addMessage = (text, time, userMessage) => {
     let messageSlot = document.createElement('DIV');
     messageSlot.className = 'col-7'
 
+    // Only thing different here is it check who sent message and formats accordingly
     let message = document.createElement('P');
     message.textContent = text;
     message.className = userMessage ? 'message user' : 'message bot';
@@ -20,6 +22,7 @@ const addMessage = (text, time, userMessage) => {
     messageTime.textContent = datetime.toLocaleTimeString();
     messageTime.className = 'font-weight-light text-right';
 
+    // Appends all elements to eachother to show the message on the DOM
     message.appendChild(messageTime);
     messageSlot.appendChild(message);
     if (userMessage) {
@@ -34,16 +37,19 @@ const addMessage = (text, time, userMessage) => {
 };
 
 const updateMessages = (messages) => {
+    // This function updates the DOM to show all new messages
     messages = messages.sort((a, b) => { return a.time < b.time; });
     let messageBox = document.getElementById('message-box');
     messageBox.innerHTML = '';
     messages.forEach(m => {
         addMessage(m.message, m.time, m.recipientID == 'b11f6ede-11c7-4705-92cb-d92785903f3d');
     });
+    // Automatically scrolls to the bottom once new message arrives
     window.scrollTo(0,document.body.scrollHeight);
 };
 
 const getMessages = (sessionID) => {
+    // This makes a request to retrieve all new messages of the current session
     // TEST SESSION '6a4c14ac-ef1a-47a6-b451-438d7d572887'
     let sessionData = {
         sessionID: sessionID
@@ -56,16 +62,18 @@ const getMessages = (sessionID) => {
         return response.json();
     }).then((data) => {
         if (data['success']) {
+            // Updates all messages if received new messages
             updateMessages(data['messages']);
         } else {
+            // If there is an issue force a logout by clearing client ID creds in storage
             alert('Issue with retrieving messages. Try logging in again.');
-            localStorage.clear();
-            window.location.href = '/';
+            logout();
         }
     });
 };
 
 const sendMessage = (clientID, sessionID, message) => {
+    // This sends a new message from the client to the AI
     let messageData = {
         message: message,
         sessionID: sessionID,
@@ -79,43 +87,56 @@ const sendMessage = (clientID, sessionID, message) => {
         return response.json();
     }).then((data) => {
         if (data['success']) {
+            // Must retrieve new messages once a message is sent because the AI
+            // will automatically and immediately respond
             let sessionID = localStorage.getItem('sessionID');
             getMessages(sessionID);
-            console.log(`Successfully sent: "${message}"`);
+            // console.log(`Successfully sent: "${message}"`);
         } else {
             alert('Issue with sending message! Try again!');
         }
     });
 };
 
+const logout = () => {
+    // Clears local creds and redirects for logout
+    localStorage.clear();
+    window.location.href = '/';
+};
+
+// Check IDs makes sure that the client ID and current messaging session ID 
+// are available in local storage for use
 const checkIDs = () => { return localStorage.getItem('clientID') && localStorage.getItem('sessionID'); };
 
+// On page load make sure to update all required current messages
 if (checkIDs) {
     let sessionID = localStorage.getItem('sessionID');
     getMessages(sessionID);
 } else {
+    // Force logout if there is a message issue
     alert('Issue with retrieving messages. Try logging in again.');
     localStorage.clear();
     window.location.href = '/';
 }
 
+// On logout force logout action
 document.getElementById('logout').addEventListener('click', () => {
-    localStorage.clear();
-    window.location.href = '/';
+    logout();
 });
 
 document.getElementById('send').addEventListener('click', () => {
+    // On a send button click we must check all required params are available
     let message = document.getElementById('message');
     if (message.value) {
         if (checkIDs) {
+            // If all params check out then we can send message with request
             let clientID = localStorage.getItem('clientID');
             let sessionID = localStorage.getItem('sessionID');
             sendMessage(clientID, sessionID, message.value);
             message.value = '';
         } else {
             alert('Issue with session and client IDs. Try logging in again.');
-            localStorage.clear();
-            window.location.href = '/';
+            logout();
         }
     } else {
         alert('Please enter a message to send!');
--- static/js/login.js
+++ static/js/login.js
@@ -1,18 +1,24 @@
+// If the user is already logged in and accesses this page they should just go straight to their chat
 let goToIfLoggedIn = '/chat';
 
+// If the client ID exists it means the user is already logged in
 if (localStorage.getItem('clientID')) {
     window.location.href = goToIfLoggedIn;
 }
 
 document.getElementById('login').addEventListener('submit', (e) => {
     e.preventDefault();
+    // Grabs login credentials
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
+    // Hashes the password on client side before sending to the server
     let loginData = {
         username: username,
         password: sha256(password)
     };
+    // If both the username and password exist then we can check to login
     if (username && password) {
+        // Make a POST request to the login endpoint with the username and password params
         fetch('/request/login', {
             method: 'POST',
             body: JSON.stringify(loginData),
@@ -21,7 +27,9 @@ document.getElementById('login').addEventListener('submit', (e) => {
             return response.json();
         }).then((data) => {
             if (data['success']) {
+                // If success in the return JSON then the login was successful and we are given params to store the clientID
                 localStorage.setItem('clientID', data['clientID']);
+                // Now that we are logged in we must start a new messaging session
                 fetch('/start', {
                     method: 'POST',
                     body: JSON.stringify({'clientID': data['clientID']}),
@@ -30,6 +38,8 @@ document.getElementById('login').addEventListener('submit', (e) => {
                     return response.json();
                 }).then((data) => {
                     if (data['success']) {
+                        // Once the request comes back we now have a new messaging session ID to use
+                        // We can now go to the chat location
                         localStorage.setItem('sessionID', data['sessionID']);
                         window.location.href = goToIfLoggedIn;
                     } else {
@@ -38,6 +48,7 @@ document.getElementById('login').addEventListener('submit', (e) => {
                     }
                 });
             } else {
+                // Must retry if login fails
                 alert('That username and/or password is incorrect!');
                 localStorage.clear();
             }
